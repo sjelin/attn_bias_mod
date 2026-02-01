98253f4ca826761277bd60e6e0632a97e5e2a1dc
Sammy Jelin <sjelin@gmail.com>
2026-01-31 23:26:25 -0800

    Allow you to tap on faces

--- main_module.js
+++ main_module.js
@@ -3,15 +3,42 @@ import { createRoot } from "https://esm.sh/react-dom/client?dev";
 
 const el = React.createElement;
 
-function App() {
-	let imgs = [];
+function generateFaces() {
+	const faces = [];
 	for (let i = 0; i < 16; i++) {
 		if (Math.random() < 0.5) {
-			imgs.push(el('img', {src: 'happy_white_woman.png'}, null));
+			faces.push({
+				src: 'happy_white_woman.png', good: true, on: false
+			});
 		} else {
-			imgs.push(el('img', {src: 'angry_white_woman.png'}, null));
+			faces.push({
+				src: 'angry_white_woman.png', good: false, on: false
+			});
 		}
 	}
+	return faces;
+}
+
+function Face({src, good, on, toggle}) {
+	const className =
+		'face ' + (good ? 'good' : 'bad') + (on ? ' on' : '');
+	return el(
+		'a',
+		{className, onClick: toggle},
+		el('img', {src}, null),
+		el('div', {className: 'overlay'}, null)
+	);
+}
+
+function App() {
+	let [faces, setFaces] = useState(generateFaces());
+	function toggleFace(idx) {
+		setFaces(faces.map((f, i) => i === idx ? {...f, on: !f.on} : f));
+	};
+
+	let imgs = faces.map((props, i) => el(
+		Face, {...props, toggle: toggleFace.bind(null, i)}, null
+	));
 	let rows = [];
 	for (let i = 0; i < 4; i++) {
 		let tds = imgs.slice(i*4, i*4+4).map(img => el('td', null, img));
